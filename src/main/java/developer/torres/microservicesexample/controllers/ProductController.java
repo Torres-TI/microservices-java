21837e79050ee9cd3d1f6bfe5c20a211c89eece7
BruninTI <bruno.araujo@viannasempre.com.br>
2023-05-04 15:55:45 -0300

    created methods to get product by id, delete product and update product in controller.

--- src/main/java/developer/torres/microservicesexample/controllers/ProductController.java
+++ src/main/java/developer/torres/microservicesexample/controllers/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -16,8 +17,21 @@ public class ProductController {
     public List<Product> getAllProducts(){
         return productService.getAllProducts();
     }
+   @GetMapping("/{id}")
+    public Optional<Product> getProductById(@PathVariable Integer id){
+        return productService.getProductById(id);
+    }
     @PostMapping
     public Product createProduct(@RequestBody  Product product){
         return productService.createProduct(product);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Integer id){
+        productService.deleteProduct(id);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@RequestBody Product product, @PathVariable Integer id){
+        return productService.updateProduct(id, product);
     }
 }
