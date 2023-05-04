251f1173900ce7e0ad9b448d4a4d19a2853e60b0
BruninTI <bruno.araujo@viannasempre.com.br>
2023-05-04 14:30:33 -0300

    Created create product controller

--- src/main/java/developer/torres/microservicesexample/controllers/ProductController.java
+++ src/main/java/developer/torres/microservicesexample/controllers/ProductController.java
@@ -3,9 +3,7 @@ package developer.torres.microservicesexample.controllers;
 import developer.torres.microservicesexample.model.Product;
 import developer.torres.microservicesexample.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,5 +15,9 @@ public class ProductController {
     @GetMapping
     public List<Product> getAllProducts(){
         return productService.getAllProducts();
+    }
+    @PostMapping
+    public Product createProduct(@RequestBody  Product product){
+        return productService.createProduct(product);
     }
 }
