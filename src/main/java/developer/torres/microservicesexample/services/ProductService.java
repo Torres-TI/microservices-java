319ba5b6066098a6254f2431afdad1846eb34305
BruninTI <bruno.araujo@viannasempre.com.br>
2023-05-04 14:00:03 -0300

    Created Product Service

---
+++ src/main/java/developer/torres/microservicesexample/services/ProductService.java
@@ -0,0 +1,63 @@
+package developer.torres.microservicesexample.services;
+
+import developer.torres.microservicesexample.model.Product;
+import developer.torres.microservicesexample.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+
+    /**
+     * Method to get all products
+     * @return products
+     */
+    public List<Product> getAllProducts(){
+        return  productRepository.getAllProducts();
+    }
+
+    /**
+     * Method to get a product by id
+     * @param id to get product
+     * @return product
+     */
+    public Optional<Product> getProductById(Integer id){
+       return productRepository.getProductById(id);
+    }
+
+    /**
+     * Method to create new product
+     * @param product to be added on list.
+     * @return  product added on list.
+     * */
+
+    public Product createProduct(Product product){
+        return productRepository.createProduct(product);
+    }
+
+    /**
+     * Method to delete a product
+     * @param id to delete
+     */
+    public void deleteProduct(Integer id){
+        productRepository.deleteProduct(id);
+    }
+
+    /**
+     * Method to update product
+     * @param product to be updated.
+     * @return product
+     */
+    public Product updateProduct(Product product){
+       return productRepository.updateProduct(product);
+    }
+
+
+}
