3f46106377fe1504b2b06ab624f2cd4febe8e5c2
Torres-TI <bruno.araujo@viannasempre.com.br>
2023-05-07 13:36:25 -0300

    local crud

---
+++ src/main/java/developer/torres/microservicesexample/repository/ProductOldRepository.java
@@ -0,0 +1,72 @@
+package developer.torres.microservicesexample.repository;
+
+import developer.torres.microservicesexample.model.Product;
+import developer.torres.microservicesexample.model.exception.ResourceNotFoundException;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductOldRepository {
+    private Integer lastId = 0;
+    private List<Product> products = new ArrayList<>();
+
+    //#region Methods
+    /**
+     * Method to get all products
+     * @return products
+     */
+    public List<Product>getAllProducts(){
+        return products;
+    }
+
+    /**
+     * Method to get a product by id
+     * @param id to get product
+     * @return product
+     */
+    public Optional<Product> getProductById(Integer id){
+        return products.stream()
+                .filter(product -> product.getId() == id)
+                .findFirst();
+    }
+
+    /**
+     * Method to create new product
+     * @param product to be added on list.
+     * @return  product added on list.
+     * */
+    public Product createProduct(Product product){
+        lastId++;
+        product.setId(lastId);
+        products.add(product);
+        return product;
+    }
+
+    /**
+     * Method to delete a product
+     * @param id to delete
+     */
+
+    public void deleteProduct(Integer id){
+        products.removeIf( product -> product.getId() == id);
+    }
+
+    /**
+     * Method to update product
+     * @param product to be updated.
+     * @return product
+     */
+    public Product updateProduct(Product product){
+       Optional<Product> hasProduct = getProductById(product.getId());
+       if(hasProduct.isEmpty()){
+           throw new ResourceNotFoundException("Product not found.");
+       }
+        deleteProduct(product.getId());
+        products.add(product);
+        return product;
+    }
+    //#endregion
+}
